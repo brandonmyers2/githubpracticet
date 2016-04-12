7aff6ca49dbfca2e257386af180607ead8ad8c0e
kingsplatter <andrew.strange@nwktc.edu>
2016-04-12 08:52:03 -0600

    i added stuff that does stuff....

--- hello/ViewController.swift
+++ hello/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    //adding code that does stuff...woot!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
