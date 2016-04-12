9272377d9263f22100f9495300c14ff4583fec18
Dominic Rogers <Dominic.Rogers@maed-6.local>
2016-04-12 08:56:19 -0600

    Commit

--- hello/ViewController.swift
+++ hello/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    //Hiding Status Bar
+    //Hiding Status Bar 
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
