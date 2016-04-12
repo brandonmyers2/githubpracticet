c44dec27af5269ff49752e56bd90b16062214ffa
Dominic Rogers <Dominic.Rogers@maed-6.local>
2016-04-12 08:53:05 -0600

    Commit

--- hello/ViewController.swift
+++ hello/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    //Hiding Status Bar
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
